00010190f5d3424d45f34f56a80047c003506402
MontiTC <143045757+MontiTC@users.noreply.github.com>
2024-03-19 22:28:07 -0700

    Update webserver.py

--- Web/P1/GoodReads/webserver.py
+++ Web/P1/GoodReads/webserver.py
@@ -7,18 +7,17 @@ from http.cookies import SimpleCookie
 import uuid
 import redis
 import os
-from bs4 import BeautifulSoup
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # Código basado en:
 # https://realpython.com/python-http-server/
 # https://docs.python.org/3/library/http.server.html
 # https://docs.python.org/3/library/http.cookies.html
-mappings =[
-    (r"^/books/(?P<book_id>\d+)$", "get_book"),
-    (r"^/$", "index"),
-    ]
+mappings ={
+        (r"^/books/(?P<book_id>\d+)$", "get_book"),
+        (r"^/$", "index"),
+        }
 
 
 class WebRequestHandler(BaseHTTPRequestHandler):
@@ -32,16 +31,14 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         session_id = None
         if not cookies:
             session_id = uuid.uuid4()
-            
         else:
             session_id = cookies["session_id"].value
-        
         return session_id
     
-    def write_session_cookie(self,session_id):
+    def write_session_cookie(self, session_id):
         cookies = SimpleCookie()
-        cookies["session_id"]=session_id
-        cookies["session_id"]["max-age"]=1000
+        cookies["session_id"] = session_id
+        cookies["session_id"]["max-age"] = 1000
         self.send_header("Set-Cookie", cookies.output(header=""))
     
     @cached_property
@@ -65,10 +62,8 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         match=re.match(pattern, path)
         if match:
           return match.groupdict()
-        else:
-          return None
+        
 
-    
     def url_mapping_response(self):
         for pattern, method in mappings:
             match = self.get_params(pattern, self.path)
@@ -86,56 +81,30 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
-        index_page = "<h1> Bienvenidos a los libros </h1>".encode("utf-8")
-        
-        self.wfile.write(index_page)
-        self.load_folder('html/books/')
+        with open('html/index.html') as f:
+            response = f.read()
+        self.wfile.write(response.encode("utf-8"))
         
     def get_book(self,book_id):
+        bookKey = f"{book_id}"
         session_id =self.get_session()
-        r.lpush(f"session:{session_id}", f"book:{book_id}")
+        
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.write_session_cookie(session_id)
         self.end_headers()
-        book_info = r.get(f"book:{book_id}") or "<h1> No existe el libro </h1>"
-        self.wfile.write(str(book_info).encode("utf-8"))
-        self.wfile.write(f"Session Id:{session_id}".encode("utf-8"))
         
-        book_list = r.lrange(f"Session Id:{session_id}",0,-1)
-        for book in book_list:
-            self.wfile.write(f"book:{book}".encode("utf-8"))
+        book_info = r.get(bookKey) or "<h1> No existe el libro </h1>".encode("utf-8")
+        self.wfile.write(book_info)
+        book_list = r.lrange(f"session: {session_id}", 0, -1)
+
+        if bookKey.encode('utf-8') not in book_list:
+            r.lpush(f"session: {session_id}", bookKey)
         
     
-        
-    def create_index(self,book_id, html):
-        soup = BeautifulSoup(html, 'html.parser')
-        texto = soup.get_text()
-        lista_texto = texto.split(' ')
-        for t in lista_texto:
-            r.sadd(t, book_id)
-
-       
-        
     def do_GET(self):
        self.url_mapping_response()
     
-    
-    
-    def load_folder(self,path):
-        files = os.listdir(path)
-        print(files)
-        for file in files:
-            match = re.match(r'^book(\d+).html$', file)
-            if match:
-                with open(path + file) as f:
-                    html = f.read()
-                    book_id = match.group(1)              
-                    r.set(book_id, html)
-                    self.create_index(book_id, html)
-                    print(match.group(0), book_id)  
-       
-       
              
     def get_response(self):
         return f"""
